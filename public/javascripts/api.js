319eaa13ddb0d667d884eb3929378645790e9d24
hosead6168 <dominique11h@yahoo.com>
2020-11-16 18:28:22 -0600

    adjusted api

--- public/javascripts/api.js
+++ public/javascripts/api.js
@@ -1,5 +1,51 @@
-function sneakers() {
+const sneakerEl = document.getElementById('sneakers');
+const privacyEl = document.getElementById('privacy');
+const baseUrlEL = document.getElementById('baseUrl');
+const disclaimerEl = document.getElementById('disclaimer');
+
+document.getElementById('btn').onclick = getSneakerInfo;
+
+
+
+function getSneakerInfo() {
     fetch('api/sneakers').then(function (data) {
         return data.json();
-    });
+    }).then(json => {
+        console.log(json)
+        obj = json;
+        sneakerEl.innerHTML = JSON.stringify(obj, undefined, 4)
+    })
+}
+
+function hideContent() {
+    // elmnt.style.display = 'contents';
+    // disclaimerEl.style.display = 'contents';
+    if (disclaimerEl.style.display === 'none') {
+        disclaimerEl.style.display = 'contents'
+    } else {
+        disclaimerEl.style.display = 'none'
+    }
+    // elmnt.style.display = 'contents';
+}
+
+function hideContent2() {
+    // elmnt.style.display = 'contents';
+    // disclaimerEl.style.display = 'contents';
+    if (baseUrlEL.style.display === 'none') {
+        baseUrlEL.style.display = 'contents'
+    } else {
+        baseUrlEL.style.display = 'none'
+    }
+    // elmnt.style.display = 'contents';
+}
+
+function hideContent3() {
+    // elmnt.style.display = 'contents';
+    // disclaimerEl.style.display = 'contents';
+    if (privacyEl.style.display === 'none') {
+        privacyEl.style.display = 'contents'
+    } else {
+        privacyEl.style.display = 'none'
+    }
+    // elmnt.style.display = 'contents';
 }
